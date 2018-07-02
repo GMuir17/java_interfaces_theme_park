533820f2552f8f9eca83471531c2f1ea119d0dad
Gary Muir <garymuir17@gmail.com>
2018-07-02 16:12:40 +0100

    sets up Park class, adds a getter for name

--- src/main/java/Attractions/AttractionTypes/Park.java
+++ src/main/java/Attractions/AttractionTypes/Park.java
@@ -1,4 +1,11 @@
 package Attractions.AttractionTypes;
 
-public class Park {
+import Attractions.Attraction;
+
+public class Park extends Attraction {
+
+    public Park(String name) {
+        super(name);
+    }
+
 }
--- src/test/java/Attractions/AttractionTypes/ParkTest.java
+++ src/test/java/Attractions/AttractionTypes/ParkTest.java
@@ -1,4 +1,22 @@
 package Attractions.AttractionTypes;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ParkTest {
+
+    Park park;
+
+    @Before
+    public void setUp() {
+        park = new Park("The Meadows");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("The Meadows", park.getName());
+    }
+
 }
