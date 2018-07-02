bf3fbabde50e051c5298d72cbc4f895afd536e5d
Gary Muir <garymuir17@gmail.com>
2018-07-02 20:25:38 +0100

    adds the minAge and minHeight properties

--- src/main/java/ThemeParks/Attractions/AttractionTypes/RollerCoaster.java
+++ src/main/java/ThemeParks/Attractions/AttractionTypes/RollerCoaster.java
@@ -1,11 +1,26 @@
 package ThemeParks.Attractions.AttractionTypes;
 
+import Behaviours.ISecurity;
 import ThemeParks.Attractions.Attraction;
 
 public class RollerCoaster extends Attraction {
 
+    private int minAge;
+    private int minHeight;
+
     public RollerCoaster(String name) {
         super(name);
+        this.minAge = 12;
+        this.minHeight = 145;
     }
+
+    public int getMinAge() {
+        return this.minAge;
+    }
+
+    public int getMinHeight() {
+        return this.minHeight;
+    }
+
 
 }
--- src/test/java/ThemeParks/Attractions/AttractionTypes/RollerCoasterTest.java
+++ src/test/java/ThemeParks/Attractions/AttractionTypes/RollerCoasterTest.java
@@ -1,5 +1,6 @@
 package ThemeParks.Attractions.AttractionTypes;
 
+import Visitors.Visitor;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,15 +9,30 @@ import static org.junit.Assert.assertEquals;
 public class RollerCoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() {
         rollerCoaster = new RollerCoaster("The Barrel Roll");
+        visitor1 = new Visitor(15, 150, 12.00);
+        visitor2 = new Visitor(18, 180, 12.00);
     }
 
     @Test
     public void hasName() {
         assertEquals("The Barrel Roll", rollerCoaster.getName());
     }
+
+    @Test
+    public void hasMinAge() {
+        assertEquals(12, rollerCoaster.getMinAge());
+    }
+
+    @Test
+    public void hasMinHeight() {
+        assertEquals(145, rollerCoaster.getMinHeight());
+    }
+
 
 }
